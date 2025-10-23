6b68cf769b180158e42f6d98fbe50064fb368dc5
Jameel Khuhro <jkhuhro110@gmail.com>
2025-10-23 11:34:05 +0500

    Add files via upload

---
+++ basics/EVEN.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	(num % 2 ==0) ? cout << "is Even" : cout << num << "is Odd";
+	return 0;
+	
+}
